65aca565ac2d61235903cda2eb72560d8d27ffce
nicozar03 <87132426+nicozar03@users.noreply.github.com>
2023-03-15 12:20:15 -0300

    fixed views and started about me

--- PROYECTO FINAL/codigo.js
+++ PROYECTO FINAL/codigo.js
@@ -83,3 +83,9 @@ function pelisDetalles(id){
     `
 })
 }
+const tween = KUTE.fromTo(
+    '#blob1',
+    { path: '#blob1' },
+    { path: '#blob2' },
+    { repeat: 999, duration: 3000, yoyo: true }
+).start();
